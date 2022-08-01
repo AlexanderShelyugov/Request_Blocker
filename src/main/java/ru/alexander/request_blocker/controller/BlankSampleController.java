d7ba21273b715446d5b16fad56990c99bafda9c3
Alexander Shelyugov <62933903+AlexanderShelyugov@users.noreply.github.com>
2022-08-01 22:27:52 +0300

    Refactor controller, use current IP provider for logging

--- src/main/java/ru/alexander/request_blocker/controller/BlankSampleController.java
+++ src/main/java/ru/alexander/request_blocker/controller/BlankSampleController.java
@@ -1,24 +1,31 @@
 package ru.alexander.request_blocker.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.ServletRequestAttributes;
+import ru.alexander.request_blocker.blocking.ip.api.CurrentIPProvider;
 import ru.alexander.request_blocker.blocking.ip.api.IPBlocks;
 
 import static org.springframework.http.ResponseEntity.ok;
-import static org.springframework.web.context.request.RequestContextHolder.currentRequestAttributes;
 
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 class BlankSampleController {
-    @GetMapping("/sample")
+    private final CurrentIPProvider ipProvider;
+
+    @GetMapping("/sample_ip_protected")
     @IPBlocks
-    public ResponseEntity<String> callSampleController() {
-        String remoteAddress = ((ServletRequestAttributes) currentRequestAttributes())
-            .getRequest().getRemoteAddr();
-        log.info("We had request from {}", remoteAddress);
-        return ok(remoteAddress);
+    public ResponseEntity<String> ipProtectedEndpoint() {
+        return ipUnprotectedEndpoint();
+    }
+
+    @GetMapping("/sample_ip_unprotected")
+    @IPBlocks
+    public ResponseEntity<String> ipUnprotectedEndpoint() {
+        log.info("We had request from {}", ipProvider.getCurrentIPAddress());
+        return ok("");
     }
 }
