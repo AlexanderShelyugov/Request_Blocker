c57de85fbdfb919c4e126681b2302e049c667cf9
Alexander Shelyugov <62933903+AlexanderShelyugov@users.noreply.github.com>
2022-08-01 23:06:59 +0300

    Refactor exception handler

--- src/main/java/ru/alexander/request_blocker/controller/IPBlockedExceptionHandler.java
+++ src/main/java/ru/alexander/request_blocker/controller/IPBlockedExceptionHandler.java
@@ -1,5 +1,6 @@
 package ru.alexander.request_blocker.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,12 +8,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import ru.alexander.request_blocker.blocking.storage.api.exceptions.TooManyRequestsByIPException;
 
 import static org.springframework.http.HttpStatus.BAD_GATEWAY;
+import static org.springframework.http.ResponseEntity.status;
 
 @ControllerAdvice
 class IPBlockedExceptionHandler {
+    private static final HttpStatus STATUS_TO_THROW = BAD_GATEWAY;
+
     @ResponseBody
     @ExceptionHandler(TooManyRequestsByIPException.class)
     public ResponseEntity<Object> tooManyIPRequests(TooManyRequestsByIPException e) {
-        return ResponseEntity.status(BAD_GATEWAY).build();
+        return status(STATUS_TO_THROW).build();
     }
 }
