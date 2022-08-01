2cd2a19d467e24a5795ac3ec88d53a68fa3ec708
Alexander Shelyugov <62933903+AlexanderShelyugov@users.noreply.github.com>
2022-08-01 06:38:01 +0300

    Init Spring Boot project

---
+++ src/main/java/ru/alexander/request_blocker/RequestBlockerApplication.java
@@ -0,0 +1,13 @@
+package ru.alexander.request_blocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestBlockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestBlockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/alexander/request_blocker/RequestBlockerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alexander.request_blocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestBlockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
