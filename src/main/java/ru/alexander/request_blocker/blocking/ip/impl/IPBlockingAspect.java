3070b756136a21c5949cc3b8f58bd86f3661419d
Alexander Shelyugov <62933903+AlexanderShelyugov@users.noreply.github.com>
2022-08-02 12:33:53 +0300

    Add health actuator

--- src/main/java/ru/alexander/request_blocker/blocking/ip/impl/IPBlockingAspect.java
+++ src/main/java/ru/alexander/request_blocker/blocking/ip/impl/IPBlockingAspect.java
@@ -5,16 +5,10 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
 import ru.alexander.request_blocker.blocking.ip.api.CurrentIPProvider;
 import ru.alexander.request_blocker.blocking.storage.api.CommonCounterLogic;
 
-import java.util.UUID;
-
 @Aspect("pertarget(ru.alexander.request_blocker.blocking.ip.impl.IPBlockingAspect.checkForRequestsPerIP())")
-@Component
-
 @RequiredArgsConstructor
 class IPBlockingAspect {
     private final int executionID;
