77a115946f135747e8a6e2abcae646edf6794f09
Alexander Shelyugov <62933903+AlexanderShelyugov@users.noreply.github.com>
2022-08-03 22:52:00 +0300

    Move Spring Web IP provider to a general package

--- src/main/java/ru/alexander/request_blocker/blocking/ip/SpringWebIPProvider.java
+++ src/main/java/ru/alexander/request_blocker/blocking/ip/SpringWebIPProvider.java
@@ -1,4 +1,4 @@
-package ru.alexander.request_blocker.blocking.ip.impl;
+package ru.alexander.request_blocker.blocking.ip;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
